cfde87fe86752966571d3ead2d32a519170c3a0f
root <root@test.localdomain>
2010-04-28 10:44:32 +0200

    adding noise/topo and rewriting simulation

---
+++ task1/gen_network.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""
+Generates a network of nodes
+with the size given in input and the connection level from 0 to 9
+"""
+
+import random
+from sys import argv, exit
+
+NUM = int(argv[1])
+CONN = int(argv[2])
+DB_RANGE = (-60, -50)
+
+if (NUM < 0 or CONN < 0 or CONN > 9):
+    print "check your arguments"
+    exit(1)
+
+for x in range(1, NUM+1):
+    for y in range(x+1, NUM+1):
+        if random.random() * CONN > 1:
+            val = random.randrange(*DB_RANGE)
+            print "%d %d %f" % (x, y, val)
+            print "%d %d %f" % (x, y, val + random.randrange(3))
--- task1/simulation.py
+++ task1/simulation.py
@@ -4,10 +4,14 @@ import sys
 import time
 from TOSSIM import *
 
+NUM_NODES = 20
+
+nodes = [t.getNode(x) for x in range(NUM_NODES)]
+
 # node with ID0 sends command
 t = Tossim([])
 n = t.getNode(32)
-n.bootAtTime(45654)
+n.bootAtTime(100)
 
 t.addChannel("Boot", sys.stdout)
 t.addChannel("BlinkC", sys.stdout)
@@ -15,3 +19,15 @@ t.addChannel("BlinkC", sys.stdout)
 for i in range(1000):
     t.runNextEvent()
 
+
+noise = open("meyer-heavy.txt", "r")
+lines = noise.readlines()
+for line in lines:
+    str = line.strip()
+    if (str != ""):
+        val = int(str)
+        for i in range(0, 7):
+            t.getNode(i).addNoiseTraceReading(val)
+
+for i in range(0, 7):
+    t.getNode(i).createNoiseModel()
