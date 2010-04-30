75039b21f5db2c3fc09bf674e0eae016ffca155f
root <andrea.crotti.0@gmail.com>
2010-04-30 23:49:51 +0200

    renaming

--- blinker/simulation.py
+++ blinker/simulation.py
@@ -1,31 +1,29 @@
 #!/usr/bin/env python
-
 import sys
 import time
 import random
-from TOSSIM import *
+from TOSSIM import Tossim
 
 RUNTIME = 200
 NUM_NODES = 100
 
-t = Tossim([])
-r = t.radio()
+sim = Tossim([])
+radio = sim.radio()
 
 # Setting up the debugging channels
 # we could make use of decorators for this for example
 # We could also use some StringIO objects to encapsulate writing on pseudo files
-t.addChannel("BlinkC", sys.stdout)
-t.addChannel("Boot", sys.stdout)
+sim.addChannel("BlinkC", sys.stdout)
+sim.addChannel("Boot", sys.stdout)
 
 # creating the list of nodes we're working with
-nodes = [t.getNode(x) for x in range(NUM_NODES)]
+nodes = [sim.getNode(x) for x in range(NUM_NODES)]
 
-# loading the topology
-# TODO: Does it make sense to open the file instead of generating directly the topology here?
+# loading the topology file
 for line in open("topo.txt"):
     vals = line.split()
     vals = (int(vals[0]), int(vals[1]), float(vals[2]))
-    r.add(*vals)
+    radio.add(*vals)
 
 for line in open("noise.txt"):
     val = int(line.strip())
@@ -35,10 +33,12 @@ for line in open("noise.txt"):
 for n in nodes:
     n.createNoiseModel()
 
+# apparently booting with fixed times could cause some problems
 for n in nodes:
     n.bootAtTime(random.randint(100001, 900009))
 
-t.runNextEvent()
-time = t.time()
-while(time + RUNTIME * 10000000000 > t.time()):
-    t.runNextEvent()
+# FIXME: why this double runNextEvent is necesary?
+sim.runNextEvent()
+time = sim.time()
+while(time + RUNTIME * 10000000000 > sim.time()):
+    sim.runNextEvent()
