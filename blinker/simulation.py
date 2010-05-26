fd37fafda7452bbf7aee16006d7558c2a2901727
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-05-26 14:12:19 +0200

    also variable inspection starts to work :)

--- blinker/simulation.py
+++ blinker/simulation.py
@@ -7,16 +7,22 @@ TODO: check what is the minimal number of events to run to be sure we trigger ev
 Usage:
 Run normally "python simulation.py", wait that the motes are booted and then, pressing C-c it will ask interactively to build a packet and will send it over the serial channel
 ow the instructions
+
+We can also inspect variables while we run the program
 """
 
 import sys
 import time
 import random
+import readline
+import rlcompleter
 
 from StringIO import StringIO
 
 from TOSSIM import *
 from SerialMsg import *
+from tinyos.tossim.TossimApp import *
+
 from gen_network import bin_tree, rand_graph
 
 RUNTIME = 2
@@ -27,21 +33,24 @@ SERIAL_PORT = 9001
 # TODO: give different colors to the various debug messages
 CHANNELS = ("Serial", "Boot", "Radio", "Routing", "Rel", "Sensor")
 
+# FIXME: still not working, the idiotic addChannel wants a real file..
 class Channel(file): 
-    def __init__(self, name, fp, color, mode='r'): 
-        super(Channel, self).__init__("/dev/null")
-        # file.__init__(path, name)
+    def __init__(self, name, fp, color, mode='w'): 
+        super(Channel, self).__init__(fp)
         self._name = name 
         self._color = color 
 
-    def write(self, message): 
+    def write(self, message):
         message = "%s: %s" % (self._name, message) 
-        self.write(message)
+        file.write(message)
         
 class Simulation(object):
     def __init__(self, num_nodes, port, channels):
+        self.app = NescApp()
+        self.vars = self.app.variables.variables()
         self.num_nodes = num_nodes
-        self.sim = Tossim([])
+        # I pass the variables to the simulator 
+        self.sim = Tossim(self.vars)
         self.nodes = [self.sim.getNode(x) for x in range(self.num_nodes)]
         self.mac = self.sim.mac()
         self.radio = self.sim.radio()
@@ -49,7 +58,8 @@ class Simulation(object):
         self.throttle = Throttle(self.sim, 10)
         self.seqno = 0
         # at the moment is only used to store and show the topology informations
-        self.topology = []
+        self.topology = {}
+        # this is used to inspect our code
 
         # adding all the channels
         for c in channels:
@@ -134,37 +144,47 @@ class Simulation(object):
             self.radio.remove(n1, n2)
             
         self.radio.add(n1, n2, distance)
-        self.topology.append((n1,n2, distance))
+        self.topology[(n1,n2)] = distance
 
     def remove_connection(self, n1, n2):
         if self.radio.connected(n1, n2):
             self.radio.remove(n1, n2)
-            self.topology.remove((n1, n2))
+            del self.topology[(n1, n2)]
         else:
             print "not present in the topology"
 
     def interactive(self):
-        choice = input("\n\n1)topology management\n2)packet creation\n")
+        choice = input("\n\n1)topology management\n2)packet creation\n3)variable inspection\n\n")
         if choice == 1:
             self.manipulate_topology()
         if choice == 2:
             self.send_packet()
+        if choice == 3:
+            self.inspect_variable()
+
+    def inspect_variable(self):
+        readline.parse_and_bind("tab: complete")
+        mote = input("which mote you want to inspect?\n")
+        c = rlcompleter.Completer(dict(zip(self.vars, self.vars)))
+        readline.set_completer(c.complete)
+        var = raw_input("which variable do you want to inspect\n")
+        print "mote %d:var %s = %s" % (mote, var, self.nodes[mote].getVariable(var).getData())
             
     def manipulate_topology(self):
         choice = input("1)see actual topology\n2)add one connection\n3)remove one connection\n")
         if choice == 1:
             for x in self.topology:
-                print "(%d -> %d) (%f)" % (x)
+                print "(%d -> %d) (%f)" % (x[0], x[1], self.topology[x])
 
         if choice == 2:
             n1, n2, dist = input("first node\n"), input("second node\n"), float(input("distance\n"))
             self.add_connection(n1, n2, dist)
 
         if choice == 3:
-            nodes = raw_input("what are the nodes to remove?\n")
+            nodes = raw_input("what are the nodes to remove write X Y?\n")
             n1, n2 = map(int, nodes.split(" "))
             if self.radio.connected(n1, n2):
-                self.radio.remove(n1, n2)
+                self.remove_connection(n1, n2)
             else:
                 print "not connected already"
         
