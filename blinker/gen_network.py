bd679b5cf269d24a3e5dcff4ae45c1bde300a587
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-06-01 19:49:28 +0200

    adding a nice function to spit out the png of the topology from the file

--- blinker/gen_network.py
+++ blinker/gen_network.py
@@ -26,7 +26,55 @@ def bin_tree(dim):
         yield (x, x * 2 + 1)
         yield (x, (x+1) * 2)
 
-# TODO: use a decorator for this usage instead
-def write_topology(topo_maker, out_file):
-    for vals in topo_maker():
-        pass
+
+def network_to_hops_parents(topo):
+    """
+    Given a topology as a list of couples return the a dictionary
+    with the minimal hops and the parent
+    """
+    parents, hops, grid = {}, {}, {}
+    for x, y in topo:
+        if x in grid:
+            grid[x].append(y)
+        else:
+            grid[x] = []
+
+        if y in grid:
+            grid[y].append(x)
+        else:
+            grid[y] = []
+
+    # we build the structure starting from node 0
+    for x in grid:
+        parents[x] = None
+        hops[x] = 255
+
+    idx = 0
+    parents[idx] = 0
+    hops[idx] = 0
+    # just use a BFS algorithm here
+
+def sim_txt_to_png(topo_file):
+    topo = []
+    for line in open(topo_file):
+        vals = line.split()
+        topo.append((int(vals[0]), int(vals[1])))
+
+    topology_to_png(topo, topo_file.split(".")[0])
+
+def topology_to_png(topology, filename):
+    try:
+        import pydot
+    except ImportError:
+        print "you need to install pydot for this"
+        return
+    
+    p = pydot.Dot()
+    for x,y in topology:
+        p.add_edge(pydot.Edge(str(x), str(y)))
+    
+    f = filename + ".png"
+    print "writing out to %s" % f
+    p.write_png(f)
+    
+
