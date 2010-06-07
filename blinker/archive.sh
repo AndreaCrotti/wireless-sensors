8c7d1cd8b53f688869bc029ee3e464895d06a807
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-06-07 14:12:39 +0200

    now archive works also for git < 1.7

--- blinker/archive.sh
+++ blinker/archive.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-git archive -v --format=tar -o task$1sensornet1.tar HEAD
+NAME="task$1sensornet1.tar"
+echo "creating the archive to $NAME"
+git archive --format=tar --output=$NAME HEAD
