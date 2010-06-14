57317e067886f05db470e333326e05f1d68e72e9
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-06-14 10:42:12 +0200

    python formatter again

--- tosthread/fmtSerial.py
+++ tosthread/fmtSerial.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
 
 import sys
-#####################################################################
-# dests: 0000, seqno: 00, instr: 01111011   (from node 00, to FFFF) #
-#####################################################################
 
-fromHex = lambda x: int(x, 16)
-# get info from the starting string
+while True:
+    try:
+        line = sys.stdin.readline() 
+        if not(line): 
+            break 
 
-while True: 
-    line = sys.stdin.readline() 
-    if not(line): 
-        break 
-
-    hexas = map(fromHex, line.split(' '))
-    print hexas
+        print "now led is %d" % int(line.split(' ')[-1], 16)
+    except KeyboardInterrupt:
+        sys.exit(0)
